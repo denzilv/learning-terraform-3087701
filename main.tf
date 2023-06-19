4ff484668b4a1304efcc8a42ea5d2f06d18c59c8
Denzil Villarico <dvillarico@netflix.com>
2023-06-19 14:41:43 -0700

    Revert "Clean up resources in favor of module for security group"
    This reverts commit 7cfde43b0ea7bbe50135bf8cb01c594d11d9eff2.

--- main.tf
+++ main.tf
@@ -47,6 +47,10 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
+<<<<<<< HEAD
+=======
+  name    = "blog_new"
+>>>>>>> parent of 7cfde43 (Clean up resources in favor of module for security group)
 
   name   = "blog"
   vpc_id = module.vpc.public_subnets[0]
@@ -55,4 +59,40 @@ module "blog_sg" {
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "blog" {
+  name = "blog"
+  description = "Allow http and https in. Allow everything out"
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
 }
